7c20369aa3dc2561d27ec86d6daa586cdf2efd1a
dzenkis <janohalagan@gmail.com>
2024-06-18 22:48:17 +0200

    cana v1.1 (details-overlay-fix, sidebar-fixed)

--- public/assets/js/overlay.js
+++ public/assets/js/overlay.js
@@ -1,7 +1,16 @@
- document.getElementById('openOverlay').addEventListener('click', function() {
-      document.getElementById('overlay').classList.remove('hidden');
+ document.getElementById('openOverlay1').addEventListener('click', function() {
+      document.getElementById('overlay1').classList.remove('hidden');
     });
 
-    document.getElementById('closeOverlay').addEventListener('click', function() {
-      document.getElementById('overlay').classList.add('hidden');
+    document.getElementById('closeOverlay1').addEventListener('click', function() {
+      document.getElementById('overlay1').classList.add('hidden');
+    });
+
+
+document.getElementById('openOverlay2').addEventListener('click', function() {
+      document.getElementById('overlay2').classList.remove('hidden');
+    });
+
+    document.getElementById('closeOverlay2').addEventListener('click', function() {
+      document.getElementById('overlay2').classList.add('hidden');
     });
